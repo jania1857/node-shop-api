a1692a3345fae7c47660880cc021569504078186
Hubert J <hubert.janiuk.2ti@gmail.com>
2023-10-24 21:33:03 +0200

    Simple authentication endpoint

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,5 +1,6 @@
 const {db} = require("../../db.connection");
 const bcrypt = require('bcrypt');
+const {response} = require("express");
 
 async function register(req, res, next) {
     const email = req.body.email;
@@ -53,7 +54,43 @@ async function deleteUser(req, res, next) {
     }
 }
 
+async function login(req, res, next) {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const result = await db.oneOrNone('SELECT * FROM users WHERE email = $1', [email]);
+        if (result == null) {
+            return res.status(401).json({
+                message: "Authentication failed"
+            })
+        } else {
+            console.log("Dupa");
+            bcrypt.compare(password, result.password, (error, resp) => {
+                if (error) {
+                    return res.status(401).json({
+                        message: "Authentication failed"
+                    })
+                }
+                if (resp) {
+                    return res.status(200).json({
+                        message: "Auth successful"
+                    })
+                }
+                return res.status(401).json({
+                    message: "Authentication failed"
+                })
+            })
+        }
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message ? error.message : 'Something went wrong'
+        });
+    }
+}
+
 module.exports = {
     register,
-    deleteUser
+    deleteUser,
+    login
 }
--- api/routes/users.js
+++ api/routes/users.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/users');
 
-router.post('/register', controller.register)
+router.post('/register', controller.register);
 
-router.delete('/:userId', controller.deleteUser)
+router.delete('/:userId', controller.deleteUser);
+
+router.post('/login', controller.login);
 
 module.exports = router;
